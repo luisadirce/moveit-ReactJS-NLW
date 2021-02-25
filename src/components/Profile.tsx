4a839b49397efd79c0f097fa1613feb1447b8aed
luisadirce.13 <luisadirce@id.uff.br>
2021-02-25 14:27:42 -0300

    First commit
    Second day's content

---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/42549670?s=400&u=5eae3ebd9b88f4f5ac3fdd85c9d7d1f5ba1946c7&v=4" alt="Luisa Dirce" />
+            <div>
+                <strong>Luisa Dirce</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level 1
+                </p>
+            </div>
+        </div>
+    );
+    
+}
